3c80ac637f17ab68e52d8d330322d7bcabf6d6b6
cmoral58 <programmingrequests@gmail.com>
2020-10-17 11:44:03 -0500

    Added Tasks Page

--- lib/pages/notes/notes_page.dart
+++ lib/pages/notes/notes_page.dart
@@ -1,13 +1,120 @@
+import 'package:Studily/data/database-helper-notes.dart';
+import 'package:Studily/pages/notes/taskpage.dart';
+import 'package:Studily/widgets.dart';
 import 'package:flutter/material.dart';
 
-class NotesPage extends StatelessWidget {
-  const NotesPage({Key key}) : super(key: key);
+class NotesPage extends StatefulWidget {
+  @override
+  _NotesPageState createState() => _NotesPageState();
+}
+
+class _NotesPageState extends State<NotesPage> {
+  DatabaseHelper _dbHelper = DatabaseHelper();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: <Widget>[Text('Notes Page')],
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          // color: Color(0xFFF6F6F6),
+          color: Colors.white,
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(
+                      top: 32.0,
+                      bottom: 32.0,
+                    ),
+                    // child: Image(
+                    //   image: AssetImage('assets/images/logo.png'),
+                    // ),
+                    child: Text(
+                      'Notes/Todos',
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: FutureBuilder(
+                      initialData: [],
+                      future: _dbHelper.getTasks(),
+                      builder: (context, snapshot) {
+                        return ScrollConfiguration(
+                          behavior: NoGlowBehaviour(),
+                          child: ListView.builder(
+                            itemCount: snapshot.data.length,
+                            itemBuilder: (context, index) {
+                              return GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => Taskpage(
+                                        task: snapshot.data[index],
+                                      ),
+                                    ),
+                                  ).then(
+                                    (value) {
+                                      setState(() {});
+                                    },
+                                  );
+                                },
+                                child: TaskCardWidget(
+                                  title: snapshot.data[index].title,
+                                  desc: snapshot.data[index].description,
+                                ),
+                              );
+                            },
+                          ),
+                        );
+                      },
+                    ),
+                  )
+                ],
+              ),
+              Positioned(
+                bottom: 24.0,
+                right: 0.0,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => Taskpage(
+                                task: null,
+                              )),
+                    ).then((value) {
+                      setState(() {});
+                    });
+                  },
+                  child: Container(
+                    width: 60.0,
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [Color(0xFF7349FE), Color(0xFF643FDB)],
+                          begin: Alignment(0.0, -1.0),
+                          end: Alignment(0.0, 1.0)),
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    child: Image(
+                      image: AssetImage(
+                        "assets/images/add_icon.png",
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
