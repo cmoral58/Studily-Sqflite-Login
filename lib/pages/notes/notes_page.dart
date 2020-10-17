06c7b8d11e548e4e807aec0cfb46a491f9678d2f
cmoral58 <programmingrequests@gmail.com>
2020-10-17 12:00:08 -0500

    Added back arrow

--- lib/pages/notes/notes_page.dart
+++ lib/pages/notes/notes_page.dart
@@ -26,21 +26,68 @@ class _NotesPageState extends State<NotesPage> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
-                    margin: EdgeInsets.only(
-                      top: 32.0,
-                      bottom: 32.0,
-                    ),
-                    // child: Image(
-                    //   image: AssetImage('assets/images/logo.png'),
-                    // ),
-                    child: Text(
-                      'Notes/Todos',
-                      style: TextStyle(
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
+                      margin: EdgeInsets.only(
+                        top: 32.0,
+                        bottom: 32.0,
                       ),
-                    ),
-                  ),
+                      // child: Image(
+                      //   image: AssetImage('assets/images/logo.png'),
+                      // ),
+                      child: Row(
+                        children: [
+                          // InkWell(
+                          //   onTap: () {
+                          //     Navigator.pop(context);
+                          //   },
+                          //   child: Padding(
+                          //     padding: const EdgeInsets.all(8.0),
+                          //     child: Image(
+                          //       image: AssetImage(
+                          //           'assets/images/back_arrow_icon.png'),
+                          //     ),
+                          //   ),
+                          // ),
+                          Builder(
+                            builder: (context) => Ink(
+                              decoration: ShapeDecoration(
+                                // color: Color(0xFF211551).withOpacity(0.2),
+                                color: Color(0xFF211551),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10)),
+                                ),
+                              ),
+                              child: IconButton(
+                                icon: Icon(
+                                  Icons.arrow_back_rounded,
+
+                                  // color: Colors.white.withOpacity(0.8),
+                                  color: Color(0xFF211551).withOpacity(0.8),
+                                  size: 30,
+                                ),
+                                onPressed: () => Navigator.pop(context),
+                              ),
+                            ),
+                          ),
+                          // FlatButton.icon(
+                          //   onPressed: () {
+                          //     Navigator.pop(context);
+                          //   },
+                          //   icon: Icon(Icons.arrow_back),
+                          //   label: Text(''),
+                          // ),
+
+                          Expanded(
+                            child: Text(
+                              'Notes/Todos',
+                              style: TextStyle(
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          )
+                        ],
+                      )),
                   Expanded(
                     child: FutureBuilder(
                       initialData: [],
